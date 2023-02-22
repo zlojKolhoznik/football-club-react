99cc595e7410d61afd14dbc4fbad4db7e00e6429
Roman Portianko <rportyanko10@gmail.com>
2023-02-22 19:22:21 +0200

    Task completed

--- src/ClubAchievements.jsx
+++ src/ClubAchievements.jsx
@@ -4,24 +4,22 @@ import "./Club.css";
 export default class ClubAchievements extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {
-            club: props.club
-        };
     }
 
     render() {
+        let { club } = this.props;
         return (
             <div>
                 <h2 className="title">Achievements</h2>
                 <ul>
                     <li className="achievement">
-                        <b>League titles: </b> {this.state.club.leagueTitles}
+                        <b>League titles: </b> {club.leagueTitles}
                     </li>
                     <li className="achievement">
-                        <b>Cups won: </b> {this.state.club.cups}
+                        <b>Cups won: </b> {club.cups}
                     </li>
                     <li className="achievement">
-                        <b>European Cups: </b> {this.state.club.europeanCups}
+                        <b>European Cups: </b> {club.europeanCups}
                     </li>
                 </ul>
             </div>
--- src/ClubMembers.jsx
+++ src/ClubMembers.jsx
@@ -4,17 +4,15 @@ import "./Club.css";
 export default class ClubMembers extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {
-            club: props.club
-        };
     }
 
     render() {
+        let { club } = this.props;
         return (
             <div>
                 <h2 className="title">Members</h2>
                 <ul>
-                    {this.state.club.members.map((member, index) => (
+                    {club.members.map((member, index) => (
                         <li key={index} className="member">{member}</li>
                     ))}
                 </ul>
